bc4efce7e641b656f471b42133cf2327b20efb70
侯震 <450351763@qq.com>
2017-02-17 17:53:58 +0800

    first

--- colorExt/ViewController.swift
+++ colorExt/ViewController.swift
@@ -12,12 +12,18 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor.init(r: 10, g: 10, b: 100)
+        self.view.backgroundColor = UIColor.init(hex: "ecccch");
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.backgroundColor = UIColor.randomColor()
+
     }
 
 
